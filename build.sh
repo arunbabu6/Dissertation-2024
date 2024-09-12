103f63700d9717378f9438b1e88c34c3589bddf6
arunbabu6 <153723482+arunbabu6@users.noreply.github.com>
2024-09-12 22:08:51 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,4 @@
 cd hello-world-html
 
 # Build the Docker image and tag it with the Jenkins build number
-docker build -t ebpfztn:${BUILD_ID} .
-
-
+docker build --output type=image,push=false --hash -t ebpfztn:${BUILD_ID} .
